148ceeeee0839c4f1afc52bf8d77fc259e20b2c4
James Hsu <jameshsu1125@gmail.com>
2025-08-17 21:29:53 +0800

    feat(button): 新增 Click 事件處理器以處理按鈕點擊，並更新樣式以支持滑動手勢

--- src/pages/home/choose/button/index.tsx
+++ src/pages/home/choose/button/index.tsx
@@ -1,9 +1,13 @@
-import { memo, useContext, useEffect } from 'react';
+import { memo, useContext, useEffect, useId } from 'react';
 import './index.less';
 import { HomeContext, HomePageType, HomeStepType } from '../../config';
 import useTween from 'lesca-use-tween';
+import Click from 'lesca-click';
+import { ChooseContext } from '../config';
 
 const Button = memo(() => {
+  const id = useId();
+  const [{ index }] = useContext(ChooseContext);
   const [{ step, page }] = useContext(HomeContext);
   const [style, setStyle] = useTween({ opacity: 0, scale: 2 });
 
@@ -13,9 +17,15 @@ const Button = memo(() => {
     }
   }, [page, step]);
 
+  useEffect(() => {
+    Click.add(`#${id}`, () => {
+      console.log(index);
+    });
+  }, [index]);
+
   return (
     <div className='Button'>
-      <div className='button' style={style}>
+      <div id={id} className='button' style={style}>
         <div>
           <div />
         </div>
