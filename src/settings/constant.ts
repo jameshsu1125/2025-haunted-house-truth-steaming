e43099550479da682cc99f3ac42cc621a75822d5
James Hsu <jameshsu1125@gmail.com>
2025-08-26 12:32:51 +0800

    feat: 調整 Zhongli 遊戲狀態，將頁面改為首頁並重置影片加載狀態

--- src/settings/constant.ts
+++ src/settings/constant.ts
@@ -17,7 +17,7 @@ export const LoadingProcessState: TLoadingProcessState = {
 };
 
 export const InitialState: IState = {
-  [ActionType.Page]: PAGE.zhongli,
+  [ActionType.Page]: PAGE.taipei,
   [ActionType.LoadingProcess]: LoadingProcessState,
   [ActionType.location]: TLocationType.taipei,
   [ActionType.smokeEffect]: false,
