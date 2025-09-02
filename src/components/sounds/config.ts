94740d0ecdf263d220cc34c81d81bb5af9322a7b
James Hsu <jameshsu1125@gmail.com>
2025-09-02 14:00:48 +0800

    feat: 重構聲音管理，將聲音配置移至 config.ts 並使用 SOUNDS_CONFIG

---
+++ src/components/sounds/config.ts
@@ -0,0 +1,50 @@
+import chooseBGM_URL from './mp3/choose-bgm.mp3';
+import cleanerURL from './mp3/cleaner.mp3';
+import clickURL from './mp3/click.mp3';
+import dishURL from './mp3/dish.mp3';
+import doorURL from './mp3/door.mp3';
+import FailURL from './mp3/fail.mp3';
+import flapURL from './mp3/flap.mp3';
+import footstepURL from './mp3/footsteps.mp3';
+import ghostURL from './mp3/ghost.mp3';
+import hintURL from './mp3/hint.mp3';
+import laughURL from './mp3/laugh.mp3';
+import lightURL from './mp3/light.mp3';
+import taipeiBGM_URL from './mp3/taipei-bgm.mp3';
+import passURL from './mp3/pass.mp3';
+import winURL from './mp3/win.mp3';
+
+export type SoundKeyType =
+  | 'chooseBGM'
+  | 'taipeiBGM'
+  | 'flap'
+  | 'click'
+  | 'hint'
+  | 'footstep'
+  | 'door'
+  | 'ghost'
+  | 'dish'
+  | 'cleaner'
+  | 'laugh'
+  | 'light'
+  | 'fail'
+  | 'pass'
+  | 'win';
+
+export const SOUNDS_CONFIG: Record<SoundKeyType, { src: string; loop: boolean; volume: number }> = {
+  chooseBGM: { src: chooseBGM_URL, loop: true, volume: 0.3 },
+  taipeiBGM: { src: taipeiBGM_URL, loop: true, volume: 0.3 },
+  flap: { src: flapURL, loop: false, volume: 1 },
+  click: { src: clickURL, loop: false, volume: 1 },
+  hint: { src: hintURL, loop: false, volume: 1 },
+  footstep: { src: footstepURL, loop: true, volume: 1 },
+  door: { src: doorURL, loop: false, volume: 1 },
+  ghost: { src: ghostURL, loop: false, volume: 1 },
+  dish: { src: dishURL, loop: false, volume: 1 },
+  cleaner: { src: cleanerURL, loop: false, volume: 1 },
+  laugh: { src: laughURL, loop: false, volume: 1 },
+  light: { src: lightURL, loop: false, volume: 1 },
+  fail: { src: FailURL, loop: true, volume: 1 },
+  pass: { src: passURL, loop: true, volume: 1 },
+  win: { src: winURL, loop: true, volume: 1 },
+};
--- src/components/sounds/index.tsx
+++ src/components/sounds/index.tsx
@@ -1,68 +1,44 @@
 import { Howl } from 'howler';
-import chooseBGM_URL from './mp3/choose-bgm.mp3';
-import cleanerURL from './mp3/cleaner.mp3';
-import clickURL from './mp3/click.mp3';
-import dishURL from './mp3/dish.mp3';
-import doorURL from './mp3/door.mp3';
-import FailURL from './mp3/fail.mp3';
-import flapURL from './mp3/flap.mp3';
-import footstepURL from './mp3/footsteps.mp3';
-import ghostURL from './mp3/ghost.mp3';
-import hintURL from './mp3/hint.mp3';
-import laughURL from './mp3/laugh.mp3';
-import lightURL from './mp3/light.mp3';
-import taipeiBGM_URL from './mp3/taipei-bgm.mp3';
-import passURL from './mp3/pass.mp3';
-import winURL from './mp3/win.mp3';
+import { SoundKeyType, SOUNDS_CONFIG } from './config';
 
 type SoundsProps = {
   onload?: () => void;
 };
 
 export default class Sounds {
-  private sounds: { [k: string]: string } = {
-    chooseBGM: chooseBGM_URL,
-    taipeiBGM: taipeiBGM_URL,
-    flap: flapURL,
-    click: clickURL,
-    hint: hintURL,
-    footstep: footstepURL,
-    door: doorURL,
-    ghost: ghostURL,
-    dish: dishURL,
-    cleaner: cleanerURL,
-    laugh: laughURL,
-    light: lightURL,
-    fail: FailURL,
-    pass: passURL,
-    win: winURL,
-  };
-
   private onload?: () => void;
-  public tracks: { [k: string]: { track: Howl; loaded: boolean } } = {};
+  public tracks: Record<SoundKeyType, { track: Howl; loaded: boolean }> = {} as Record<
+    SoundKeyType,
+    { track: Howl; loaded: boolean }
+  >;
 
   constructor({ onload }: SoundsProps) {
     this.onload = onload;
     this.load();
   }
 
-  onSoundLoaded(key: string) {
+  onSoundLoaded(key: SoundKeyType) {
     this.tracks[key].loaded = true;
     this.checkAllSoundsLoaded();
   }
 
   checkAllSoundsLoaded() {
     const howManySoundsLoaded = Object.values(this.tracks).filter((data) => data.loaded);
-    const howManySoundsShouldLoaded = Object.keys(this.sounds).length;
+    const howManySoundsShouldLoaded = Object.keys(SOUNDS_CONFIG).length;
     if (howManySoundsLoaded.length === howManySoundsShouldLoaded) {
       this.onload?.();
     }
   }
 
   load() {
-    Object.entries(this.sounds).forEach(([key, src]) => {
-      const sound = new Howl({ src: [src], onload: () => this.onSoundLoaded(key) });
-      this.tracks[key] = { track: sound, loaded: false };
+    Object.entries(SOUNDS_CONFIG).forEach(([key, config]) => {
+      const sound = new Howl({
+        src: [config.src],
+        loop: config.loop,
+        volume: config.volume,
+        onload: () => this.onSoundLoaded(key as SoundKeyType),
+      });
+      this.tracks[key as SoundKeyType] = { track: sound, loaded: false };
     });
   }
 }
