c4cb58554ab62fb6a8e5347fbb6943f9931a7b66
James Hsu <jameshsu1125@gmail.com>
2025-09-04 11:50:26 +0800

    fix: 調整各頁面樣式，將最小高度設置為全高以改善佈局

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -5,7 +5,7 @@ import Div100vh from 'react-div-100vh';
 const Container = memo(({ children }: IReactProps) => (
   <Div100vh className='from-backgroundGrey to-backgroundColor w-full bg-gradient-to-b'>
     <div className='flex h-full w-full justify-center overflow-y-auto'>
-      <div className='bg-background relative h-full w-full max-w-2xl'>{children}</div>
+      <div className='bg-background relative h-full min-h-full w-full max-w-2xl'>{children}</div>
     </div>
   </Div100vh>
 ));
